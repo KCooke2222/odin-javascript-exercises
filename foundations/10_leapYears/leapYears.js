f17470c6c0aa2ec2228fd8c7fea8b1fe31ff6e48
KCooke2222 <136854995+KCooke2222@users.noreply.github.com>
2026-01-04 12:32:38 -0800

    Complete foundations to 11

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if (yr % 4 != 0) {
+        return false
+    }
+    else if (yr % 100 === 0) {
+        if (yr % 400 === 0) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line

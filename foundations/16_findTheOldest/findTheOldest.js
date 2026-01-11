817d07bfbf395622b1b2341422b124f3d966d5f1
KCooke2222 <136854995+KCooke2222@users.noreply.github.com>
2026-01-10 16:51:29 -0800

    Complete foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr = arr.sort((a, b) => {
+        if (!("yearOfDeath" in a)) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
 
+        if (!("yearOfDeath" in b)) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    });
+    console.log(arr);
+    return arr[0];
 };
 
 // Do not edit below this line

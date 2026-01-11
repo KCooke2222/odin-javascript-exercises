52fa5e4c6e6c98cb2355c15905a6d456e6cc73e6
KCooke2222 <136854995+KCooke2222@users.noreply.github.com>
2026-01-10 16:51:48 -0800

    Complete foundations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = [...str]
+        .filter(c => /[a-z0-9]/i.test(c))
+        .map(c => c.toLowerCase());
+    
+    n = str.length;
+    for (let i = 0; i < n / 2; i++) {
+        if (str[i] != str[n - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = Number(n);
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n === 0) {
+        return 0;
+    }
 
+    res = 1
+    prev = 0
+    for (let i = 1; i < n; i++) {
+        tmp = res;
+        res += prev;
+        prev = tmp;
+    }
+    return res;
 };
 
 // Do not edit below this line
